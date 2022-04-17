9d30af968deb1d7b59c2845e2bb1c0533be2c224
LT-boy <aurimas.jasius@gmail.com>
2022-04-17 13:27:17 +0300

    Create playRound function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+// Randomly returned values: Rock, Paper or Scissors
 function computerPlay() {
 	if (Math.floor(Math.random() * 3) === 0) {
 		return "Rock";
@@ -9,4 +9,34 @@ function computerPlay() {
 	}
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+		let result = "It's a draw! " + playerSelection + " and " + computerSelection;
+		return result;
+	} else if ((playerSelection.toLowerCase() == "rock") && (computerSelection.toLowerCase() == "scissors")) {
+		let result = "You win! " + playerSelection + " beats " + computerSelection;
+		return result;
+	} else if ((playerSelection.toLowerCase() == "paper") && (computerSelection.toLowerCase() == "rock")) {
+		let result = "You win! " + playerSelection + " beats " + computerSelection;
+		return result;
+	} else if ((playerSelection.toLowerCase() == "scissors") && (computerSelection.toLowerCase() == "paper")) {
+		let result = "You win! " + playerSelection + " beat " + computerSelection;
+		return result;
+	} else if ((playerSelection.toLowerCase() == "rock") && (computerSelection.toLowerCase() == "paper")) {
+		let result = "You lose! " + computerSelection + " beats " + playerSelection;
+		return result;
+	} else if ((playerSelection.toLowerCase() == "paper") && (computerSelection.toLowerCase() == "scissors")) {
+		let result = "You lose! " + computerSelection + " beat " + playerSelection;
+		return result;
+	} else if ((playerSelection.toLowerCase() == "scissors") && (computerSelection.toLowerCase() == "rock")) {
+		let result = "You lose! " + computerSelection + " beats " + playerSelection;
+		return result;
+	} else {
+		let result = "Incorrect selection!"
+		return result;
+	}
+}
+
+
+
+console.log(playRound(prompt("Make your selection (Rock, Paper, Scissors): "), computerPlay()));
