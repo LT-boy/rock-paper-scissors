868265fbef0644cd6a70202f05d2288d77d29f85
LT-boy <aurimas.jasius@gmail.com>
2022-04-17 16:37:06 +0300

    Capitalize first letter function

--- javascript.js
+++ javascript.js
@@ -9,34 +9,41 @@ function computerPlay() {
 	}
 }
 
+
 // Inputs player and computer selection values and returns result
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-		let result = "It's a draw! " + playerSelection + " and " + computerSelection;
+		let result = "It's a draw! " + capitalizeFirstLetter(playerSelection) + " and " + computerSelection;
 		return result;
 	} else if ((playerSelection.toLowerCase() == "rock") && (computerSelection.toLowerCase() == "scissors")) {
-		let result = "You win! " + playerSelection + " beats " + computerSelection;
+		let result = "You win! " + capitalizeFirstLetter(playerSelection) + " beats " + computerSelection;
 		return result;
 	} else if ((playerSelection.toLowerCase() == "paper") && (computerSelection.toLowerCase() == "rock")) {
-		let result = "You win! " + playerSelection + " beats " + computerSelection;
+		let result = "You win! " + capitalizeFirstLetter(playerSelection) + " beats " + computerSelection;
 		return result;
 	} else if ((playerSelection.toLowerCase() == "scissors") && (computerSelection.toLowerCase() == "paper")) {
-		let result = "You win! " + playerSelection + " beat " + computerSelection;
+		let result = "You win! " + capitalizeFirstLetter(playerSelection) + " beat " + computerSelection;
 		return result;
 	} else if ((playerSelection.toLowerCase() == "rock") && (computerSelection.toLowerCase() == "paper")) {
-		let result = "You lose! " + computerSelection + " beats " + playerSelection;
+		let result = "You lose! " + computerSelection + " beats " + capitalizeFirstLetter(playerSelection);
 		return result;
 	} else if ((playerSelection.toLowerCase() == "paper") && (computerSelection.toLowerCase() == "scissors")) {
-		let result = "You lose! " + computerSelection + " beat " + playerSelection;
+		let result = "You lose! " + computerSelection + " beat " + capitalizeFirstLetter(playerSelection);
 		return result;
 	} else if ((playerSelection.toLowerCase() == "scissors") && (computerSelection.toLowerCase() == "rock")) {
-		let result = "You lose! " + computerSelection + " beats " + playerSelection;
+		let result = "You lose! " + computerSelection + " beats " + capitalizeFirstLetter(playerSelection);
 		return result;
 	} else {
 		let result = "Incorrect selection! Try again."
 		return result;
 	}
 }
+
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 
 // Main game function to play 5 rounds, show round and calculate score
 function game() {
@@ -45,22 +52,30 @@ function game() {
 	for (let i = 0; i < 5; i++) {
 		result = playRound(prompt("Make your selection (Rock, Paper, Scissors): "), computerPlay());
 		if (result.includes("win")) {
+			console.log("Round: " + (i + 1) + " of 5");
 			console.log(result);
-			console.log("Round: " + (i + 1) + " of 5. Score: " + (yourScore + 1) + " - " + computerScore);
+			console.log("Score: " + (yourScore + 1) + " - " + computerScore);
 			yourScore = yourScore + 1;
 		} else if (result.includes("lose")) {
+			console.log("Round: " + (i + 1) + " of 5");
 			console.log(result);
-			console.log("Round: " + (i + 1) + " of 5. Score: " + yourScore + " - " + (computerScore + 1));
+			console.log("Score: " + yourScore + " - " + (computerScore + 1));
 			computerScore = computerScore + 1;
 		} else if (result.includes("draw")) {
+			console.log("Round: " + (i + 1) + " of 5");
 			console.log(result);
-			console.log("Round: " + (i + 1) + " of 5. Score: " + yourScore + " - " + computerScore);
+			console.log("Score: " + (yourScore + 1) + " - " + (computerScore + 1));
+			yourScore = yourScore + 1;
+			computerScore = computerScore + 1;
 		} else {
+			console.log("Round: " + (i + 1) + " of 5");
 			console.log(result);
-			console.log("Round: " + (i + 1) + " of 5. Score: " + yourScore + " - " + computerScore);
+			console.log("Score: " + yourScore + " - " + computerScore);
 			i = i - 1;
 		}
 	}
 }
 
+
+// Run game
 game()
