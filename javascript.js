4d1a5486c542643bd8e009ab0a4d5d98383853a0
LT-boy <aurimas.jasius@gmail.com>
2022-05-07 20:28:59 +0300

    Show result text

--- javascript.js
+++ javascript.js
@@ -28,15 +28,15 @@ function playRound(playerSelection, computerSelection) {
 		playerScore += 1;
 		return result;
 	} else if ((playerSelection.toLowerCase() == "rock") && (computerSelection.toLowerCase() == "paper")) {
-		let result = "You lose! " + computerSelection + " beats " + capitalizeFirstLetter(playerSelection);
+		let result = "You lose.. " + computerSelection + " beats " + capitalizeFirstLetter(playerSelection);
 		computerScore += 1;
 		return result;
 	} else if ((playerSelection.toLowerCase() == "paper") && (computerSelection.toLowerCase() == "scissors")) {
-		let result = "You lose! " + computerSelection + " beat " + capitalizeFirstLetter(playerSelection);
+		let result = "You lose.. " + computerSelection + " beat " + capitalizeFirstLetter(playerSelection);
 		computerScore += 1;
 		return result;
 	} else if ((playerSelection.toLowerCase() == "scissors") && (computerSelection.toLowerCase() == "rock")) {
-		let result = "You lose! " + computerSelection + " beats " + capitalizeFirstLetter(playerSelection);
+		let result = "You lose.. " + computerSelection + " beats " + capitalizeFirstLetter(playerSelection);
 		computerScore += 1;
 		return result;
 	} else {
@@ -51,47 +51,6 @@ function capitalizeFirstLetter(string) {
 }
 
 
-// // Main game function to play 5 rounds, show round and calculate score
-// function game(selection) {
-// 	let yourScore = 0;
-// 	let computerScore = 0;
-// 	for (let i = 0; i < 5; i++) {
-// 		result = playRound(selection, computerPlay());
-// 		if (result.includes("win")) {
-// 			console.log("Round: " + (i + 1) + " of 5");
-// 			console.log(result);
-// 			console.log("Score: " + (yourScore + 1) + " - " + computerScore);
-// 			yourScore = yourScore + 1;
-// 		} else if (result.includes("lose")) {
-// 			console.log("Round: " + (i + 1) + " of 5");
-// 			console.log(result);
-// 			console.log("Score: " + yourScore + " - " + (computerScore + 1));
-// 			computerScore = computerScore + 1;
-// 		} else if (result.includes("tie")) {
-// 			console.log("Round: " + (i + 1) + " of 5");
-// 			console.log(result);
-// 			console.log("Score: " + (yourScore) + " - " + (computerScore));
-// 		} else {
-// 			console.log("Round: " + (i + 1) + " of 5");
-// 			console.log(result);
-// 			console.log("Score: " + yourScore + " - " + computerScore);
-// 			i = i - 1;
-// 		}
-// 	}
-// 	console.log("*************************");
-// 	console.log("Final score: " + yourScore + " - " + computerScore);
-// 	if (yourScore > computerScore) {
-// 		console.log("You win the game!!!");
-// 		console.log("*************************");
-// 	} else if (yourScore < computerScore) {
-// 		console.log("You lose...");
-// 		console.log("*************************");
-// 	} else {
-// 		console.log("It's a tie.");
-// 		console.log("*************************");
-// 	}
-// }
-
 let playerScore = 0;
 let computerScore = 0;
 
@@ -99,33 +58,53 @@ addScore(playerScore, computerScore);
 
 // Run playRound function when user clicks a button
 document.getElementById("rock-btn").addEventListener("click", function() {
-	if(playerScore === 5 || computerScore === 5) {
-		console.log("end");
-	} else {
-		console.log(playRound("rock", computerPlay()));
+		document.getElementById("result-text").innerHTML = playRound("rock", computerPlay());
 		addScore(playerScore, computerScore);
-	}
+		if(playerScore >= 5) {
+			document.getElementById("final-result").innerHTML = "Congratulations! You WIN the game!";
+			document.getElementById("result-text").innerHTML = "";
+		}
+		else if(computerScore >= 5) {
+			document.getElementById("final-result").innerHTML = "Sorry, you LOST the game.. Maybe next time.";
+			document.getElementById("result-text").innerHTML = "";
+		}
+		else {
+		}
 });
 document.getElementById("paper-btn").addEventListener("click", function() {
-	if(playerScore === 5 || computerScore === 5) {
-		console.log("end");
-	} else {
-		playRound("paper", computerPlay());
+		document.getElementById("result-text").innerHTML = playRound("paper", computerPlay());
 		addScore(playerScore, computerScore);
-	}
+		if(playerScore >= 5) {
+			document.getElementById("final-result").innerHTML = "Congratulations! You WIN the game!";
+			document.getElementById("result-text").innerHTML = "";
+		}
+		else if(computerScore >= 5) {
+			document.getElementById("final-result").innerHTML = "Sorry, you LOST the game.. Maybe next time.";
+			document.getElementById("result-text").innerHTML = "";
+		}
+		else {
+		}
 });
 document.getElementById("scissors-btn").addEventListener("click", function() {
-	if(playerScore === 5 || computerScore === 5) {
-		console.log("end");
-	} else {
-		playRound("scissors", computerPlay());
+		document.getElementById("result-text").innerHTML = playRound("scissors", computerPlay());
 		addScore(playerScore, computerScore);
-	}
+		if(playerScore >= 5) {
+			document.getElementById("final-result").innerHTML = "Congratulations! You WIN the game!";
+			document.getElementById("result-text").innerHTML = "";
+		}
+		else if(computerScore >= 5) {
+			document.getElementById("final-result").innerHTML = "Sorry, you LOST the game.. Maybe next time.";
+			document.getElementById("result-text").innerHTML = "";
+		}
+		else {
+		}
 });
 
 
 function addScore(playerScore, computerScore) {
-	document.getElementById("score").innerHTML = "Player" + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" + playerScore + " : " + computerScore + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" + "Computer";
+	if(playerScore <= 5 && computerScore <= 5) {
+		document.getElementById("score").innerHTML = "Player" + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" + playerScore + " : " + computerScore + "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp" + "Computer";
+	}
 }
 
 
